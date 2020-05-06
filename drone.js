b3fe14f0e1be7121730571a4307c8e1eeef756de
Akissan <astellakissan@gmail.com>
2020-05-06 19:36:39 +0300

    Init stat assigment is removed

--- drone.js
+++ drone.js
@@ -9,137 +9,137 @@ let desSpeed;
 let drone_img = [];
 
 class Drone extends Entity {
-  constructor(pos = createVector(0, 0), name = "Default", colRad = 35, maxSpeed = 4) {
-    super("Drone", colRad, pos, 0, name);
-    this.curSpeed = 0;
-    this.velvec = createVector(0, 0);
-    this.visualDir = createVector(0, 0);
-    this.isGathering = false;
-    this.isDeploying = false;
-    this.target = null;
-    this.discoveredTargets = [];
-    //
-    this.maxSpeed = maxSpeed;
-    this.maxAccel = 0.15;
-    this.maxTrash = 4;
-    this.radarRad = 120;
-    //
-    this.communicateRad = 90;
-    this.bannedTargets = [];
-  }
-
-  setTarget(target) {
-    this.target = target;
-  }
-
-  steer() {
-    toTarget = p5.Vector.sub(this.target.pos, this.pos);
-    toTargetNormalized = toTarget.copy().normalize();
-    this.velvec.lerp(toTargetNormalized, maxSteeringForce);
-    this.visualDir.lerp(toTargetNormalized, maxVisRotForce);
-    if (toTarget.magSq() > approachDistanceSq) {
-      desSpeed = this.maxSpeed;
-    } else {
-      desSpeed = lerp(0, this.maxSpeed, (this.target && this.target == this.collideWith) ? 0 : (toTarget.mag() / approachDistance));
+    constructor(pos = createVector(0, 0), name = "Default", colRad = 35, maxSpeed = 4) {
+        super("Drone", colRad, pos, 0, name);
+        this.curSpeed = 0;
+        this.velvec = createVector(0, 0);
+        this.visualDir = createVector(0, 0);
+        this.isGathering = false;
+        this.isDeploying = false;
+        this.target = null;
+        this.discoveredTargets = [];
+        //
+        this.maxSpeed = maxSpeed;
+        this.maxAccel = 0.15;
+        this.maxTrash = 4;
+        this.radarRad = 120;
+        //
+        this.communicateRad = 90;
+        this.bannedTargets = [];
     }
-    this.curSpeed = lerp(this.curSpeed, desSpeed, this.maxAccel);
-  }
 
-  move() {
-    this.pos.add(p5.Vector.mult(this.velvec, this.curSpeed));
-  }
-
-  lookForTarget() {
-    let trashInTargets = getTrashInTargets(this.discoveredTargets);
-    let mod = 0;
-    while (trashInTargets < this.maxTrash && mod < 600) {
-      let newTargets = getInRadius(this.pos, this.radarRad + mod, beacons);
-      uniquePush(this.discoveredTargets, newTargets);
-      trashInTargets = getTrashInTargets(this.discoveredTargets);
-      mod += 15;
+    setTarget(target) {
+        this.target = target;
     }
-    this.target = findNearest(this.discoveredTargets, this.maxTrash - this.trash, this.pos, false, this.bannedTargets);
-  }
 
-  lookForBin() {
-    this.target = findNearest(bins, Number.POSITIVE_INFINITY, this.pos, true);
-  }
-
-  communicate() {
-    if (this.target && this.target.entityType == "Beacon") {
-      let nearbyDrones = getInRadius(this.pos, this.communicateRad, drones);
-      nearbyDrones.forEach(ent => {
-        if (ent != this) {
-          ent.ban(this.target)
-        };
-      });
-    }
-  }
-
-  ban(target) {
-    uniquePush(this.bannedTargets, [target]);
-    if (target == this.target) {
-      this.lookForTarget();
-    }
-  }
-
-  checkCollide() {
-    let anyGather = false;
-    let anyDeploy = false;
-    this.collideWith.forEach(ent => {
-      if (ent.entityType == "Bin") {
-        if (this.trash >= 0.001) {
-          anyDeploy = true;
-          transferTrash(ent, this, 0.1);
-        } else if (ent == this.target) {
-          this.target = null;
+    steer() {
+        toTarget = p5.Vector.sub(this.target.pos, this.pos);
+        toTargetNormalized = toTarget.copy().normalize();
+        this.velvec.lerp(toTargetNormalized, maxSteeringForce);
+        this.visualDir.lerp(toTargetNormalized, maxVisRotForce);
+        if (toTarget.magSq() > approachDistanceSq) {
+            desSpeed = this.maxSpeed;
+        } else {
+            desSpeed = lerp(0, this.maxSpeed, (this.target && this.target == this.collideWith) ? 0 : (toTarget.mag() / approachDistance));
         }
-      } else if (ent.entityType == "Beacon") {
-        if (ent.trash >= 0.0001 && this.trash < this.maxTrash) {
-          anyGather = true;
-          transferTrash(this, ent, 0.025);
-          uniquePush(this.discoveredTargets, [ent]);
-        } else if (ent == this.target) {
-          this.target = null;
+        this.curSpeed = lerp(this.curSpeed, desSpeed, this.maxAccel);
+    }
+
+    move() {
+        this.pos.add(p5.Vector.mult(this.velvec, this.curSpeed));
+    }
+
+    lookForTarget() {
+        let trashInTargets = getTrashInTargets(this.discoveredTargets);
+        let mod = 0;
+        while (trashInTargets < this.maxTrash && mod < 600) {
+            let newTargets = getInRadius(this.pos, this.radarRad + mod, beacons);
+            uniquePush(this.discoveredTargets, newTargets);
+            trashInTargets = getTrashInTargets(this.discoveredTargets);
+            mod += 15;
         }
-      }
-    })
-    this.isGathering = anyGather;
-    this.isDeploying = anyDeploy;
-  }
+        this.target = findNearest(this.discoveredTargets, this.maxTrash - this.trash, this.pos, false, this.bannedTargets);
+    }
 
-  act() {
-    this.communicate();
-    this.checkCollide();
-    if (!this.isGathering && !this.isDeploying && !this.target) {
-      this.lookForTarget();
+    lookForBin() {
+        this.target = findNearest(bins, Number.POSITIVE_INFINITY, this.pos, true);
     }
-    if (!this.target) {
-      this.lookForBin();
-    }
-    if (this.target) {
-      this.steer();
-      this.move();
-    }
-    this.bannedTargets = [];
-  }
 
-  draw() {
-    if (this.curSpeed < 0.1) {
-      image(drone_img_idle, this.pos.x - 35, this.pos.y - 35, 70, 70);
-    } else {
-      let dir = degrees(this.visualDir.heading());
-      let indx = Math.round(dir / 45);
-      if (indx < 0) {
-        indx = 8 - abs(indx);
-      }
-      image(drone_img[indx], this.pos.x - 35, this.pos.y - 35, 70, 70);
+    communicate() {
+        if (this.target && this.target.entityType == "Beacon") {
+            let nearbyDrones = getInRadius(this.pos, this.communicateRad, drones);
+            nearbyDrones.forEach(ent => {
+                if (ent != this) {
+                    ent.ban(this.target)
+                };
+            });
+        }
     }
-    noFill();
-    stroke(1);
-    rect(this.pos.x - 15, this.pos.y + 25, 30, 6);
-    noStroke();
-    fill(255, 60, 0);
-    rect(this.pos.x - 14, this.pos.y + 26, Math.round(28 * this.trash / this.maxTrash), 4);
-  }
+
+    ban(target) {
+        uniquePush(this.bannedTargets, [target]);
+        if (target == this.target) {
+            this.lookForTarget();
+        }
+    }
+
+    checkCollide() {
+        let anyGather = false;
+        let anyDeploy = false;
+        this.collideWith.forEach(ent => {
+            if (ent.entityType == "Bin") {
+                if (this.trash >= 0.001) {
+                    anyDeploy = true;
+                    transferTrash(ent, this, 0.1);
+                } else if (ent == this.target) {
+                    this.target = null;
+                }
+            } else if (ent.entityType == "Beacon") {
+                if (ent.trash >= 0.0001 && this.trash < this.maxTrash) {
+                    anyGather = true;
+                    transferTrash(this, ent, 0.025);
+                    uniquePush(this.discoveredTargets, [ent]);
+                } else if (ent == this.target) {
+                    this.target = null;
+                }
+            }
+        })
+        this.isGathering = anyGather;
+        this.isDeploying = anyDeploy;
+    }
+
+    act() {
+        this.communicate();
+        this.checkCollide();
+        if (!this.isGathering && !this.isDeploying && !this.target) {
+            this.lookForTarget();
+        }
+        if (!this.target) {
+            this.lookForBin();
+        }
+        if (this.target) {
+            this.steer();
+            this.move();
+        }
+        this.bannedTargets = [];
+    }
+
+    draw() {
+        if (this.curSpeed < 0.1) {
+            image(drone_img_idle, this.pos.x - 35, this.pos.y - 35, 70, 70);
+        } else {
+            let dir = degrees(this.visualDir.heading());
+            let indx = Math.round(dir / 45);
+            if (indx < 0) {
+                indx = 8 - abs(indx);
+            }
+            image(drone_img[indx], this.pos.x - 35, this.pos.y - 35, 70, 70);
+        }
+        noFill();
+        stroke(1);
+        rect(this.pos.x - 15, this.pos.y + 25, 30, 6);
+        noStroke();
+        fill(255, 60, 0);
+        rect(this.pos.x - 14, this.pos.y + 26, Math.round(28 * this.trash / this.maxTrash), 4);
+    }
 }
--- sketch.js
+++ sketch.js
@@ -7,77 +7,76 @@ const bins = [];
 let inp = 0;
 
 function preload() {
-  load_drone_imgs();
-  drone_img_idle = loadImage('drone_sprites/idle.png');
-  beacon_img = load_animation('bouy');
-  bin_img = load_animation('bin');
+    load_drone_imgs();
+    drone_img_idle = loadImage('drone_sprites/idle.png');
+    beacon_img = load_animation('bouy');
+    bin_img = load_animation('bin');
 }
 
 const load_drone_imgs = () => {
-  drone_img.push(loadImage('drone_sprites/E.png'));
-  drone_img.push(loadImage('drone_sprites/SE.png'));
-  drone_img.push(loadImage('drone_sprites/S.png'));
-  drone_img.push(loadImage('drone_sprites/SW.png'));
-  drone_img.push(loadImage('drone_sprites/W.png'));
-  drone_img.push(loadImage('drone_sprites/NW.png'));
-  drone_img.push(loadImage('drone_sprites/N.png'));
-  drone_img.push(loadImage('drone_sprites/NE.png'));
+    drone_img.push(loadImage('drone_sprites/E.png'));
+    drone_img.push(loadImage('drone_sprites/SE.png'));
+    drone_img.push(loadImage('drone_sprites/S.png'));
+    drone_img.push(loadImage('drone_sprites/SW.png'));
+    drone_img.push(loadImage('drone_sprites/W.png'));
+    drone_img.push(loadImage('drone_sprites/NW.png'));
+    drone_img.push(loadImage('drone_sprites/N.png'));
+    drone_img.push(loadImage('drone_sprites/NE.png'));
 }
 
 const load_animation = (name) => {
-  let load = [];
-  for (let i = 1; i < 11; i++) {
-    load.push(loadImage(`${name}_sprites/${i}.png`))
-  }
-  return load;
+    let load = [];
+    for (let i = 1; i < 11; i++) {
+        load.push(loadImage(`${name}_sprites/${i}.png`))
+    }
+    return load;
 }
 
 let button;
 
 function setup() {
-  init_stats();
-  refreshOnSpawn = true;
-  createCanvas(1000, 600);
-  textAlign(CENTER);
-  bcg = color("#ABC5CC");
-  drones.push(new Drone(randomScrPoint(20), "Fox"));
-  bins.push(new Bin(createVector(42, 42), "Binya"));
+    refreshOnSpawn = true;
+    createCanvas(1000, 600);
+    textAlign(CENTER);
+    bcg = color("#ABC5CC");
+    drones.push(new Drone(randomScrPoint(20), "Fox"));
+    bins.push(new Bin(createVector(42, 42), "Binya"));
 
-  button = createButton("");
-  button.addClass("modeButton");
-  button.addClass(`mode_${inp}`);
-  button.position(width - 100, height - 100);
-  button.mousePressed(changeInput);
+    button = createButton("");
+    button.addClass("modeButton");
+    button.addClass(`mode_${inp}`);
+    button.position(width - 100, height - 100);
+    button.mousePressed(changeInput);
 }
 
 const changeInput = () => {
-  button.removeClass(`mode_${inp}`);
-  inp == 2 ? inp = 0 : inp += 1;
-  button.addClass(`mode_${inp}`);
+    button.removeClass(`mode_${inp}`);
+    inp == 2 ? inp = 0 : inp += 1;
+    button.addClass(`mode_${inp}`);
 }
 
 function mousePressed() {
-  if (mouseX <= width && mouseX >= 0 && mouseY <= height - 100 && mouseY >= 0) {
-    switch (inp) {
-      case 0:
-        beacons.push(new Beacon(createVector(mouseX, mouseY), `New`));
-        break;
-      case 1:
-        drones.push(new Drone(createVector(mouseX, mouseY), `New`));
-        break;
-      case 2:
-        bins.push(new Bin(createVector(mouseX, mouseY), `New`));
-        break;
+    if (mouseX <= width && mouseX >= 0 && mouseY <= height - 100 && mouseY >= 0) {
+        switch (inp) {
+            case 0:
+                beacons.push(new Beacon(createVector(mouseX, mouseY), `New`));
+                break;
+            case 1:
+                drones.push(new Drone(createVector(mouseX, mouseY), `New`));
+                break;
+            case 2:
+                bins.push(new Bin(createVector(mouseX, mouseY), `New`));
+                break;
+        }
+        drones.forEach(d => d.lookForTarget());
     }
-    drones.forEach(d => d.lookForTarget());
-  }
 }
 
 function draw() {
-  background(bcg);
-  collideArrays(drones, [...beacons, ...bins]);
-  act_array(drones);
-  //draw_collide_array([...drones, ...beacons, ...bins]);
-  draw_array([...beacons, ...bins, ...drones]);
-  tick_array([...beacons]);
+    background(bcg);
+    collideArrays(drones, [...beacons, ...bins]);
+    act_array(drones);
+    //draw_collide_array([...drones, ...beacons, ...bins]);
+    draw_array([...beacons, ...bins, ...drones]);
+    tick_array([...beacons]);
 }
